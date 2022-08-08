ef1c529142a58e600c389e170584e0ae7941d0b5
студент <ilagorbunov371@gmail.com>
2022-08-08 16:20:56 +0400

    Add Open

--- TextEditor/Form1.cs
+++ TextEditor/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -14,9 +15,25 @@ namespace TextEditor
     {
         public Form1()
         {
+
             InitializeComponent();
+
+        }
+
+        private void открытьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            string filename = openFileDialog1.FileName;
+            string fileText = File.ReadAllText(filename);
+            richTextBox1.Text = fileText;
+            MessageBox.Show("Файл открыт");
         }
 
 
-    }
+          
+        
+            
+
+     }
 }
